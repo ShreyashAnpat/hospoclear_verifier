a2aad467ee4157921acc1ae2a25cf8d367c622cc
silent-codder <ajinkyashinde9799@gmail.com>
2021-03-13 14:14:33 +0530

    complete multi doctor reg

--- app/src/main/java/com/e/hospoclear/Fragments/HomeFragment.java
+++ app/src/main/java/com/e/hospoclear/Fragments/HomeFragment.java
@@ -3,12 +3,14 @@ package com.e.hospoclear.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.e.hospoclear.R;
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * 12-03-2021 (Ajinkya Shinde)
@@ -16,10 +18,14 @@ import com.e.hospoclear.R;
  */
 public class HomeFragment extends Fragment {
 
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
     }
 }
