01e343cb988ff3ad2a17aae0437a21231f335e21
silent-codder <ajinkyashinde9799@gmail.com>
2021-03-13 14:18:05 +0530

    home fragment change

--- app/src/main/java/com/e/hospoclear/Fragments/HomeFragment.java
+++ app/src/main/java/com/e/hospoclear/Fragments/HomeFragment.java
@@ -5,11 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-<<<<<<< HEAD
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
-=======
->>>>>>> a2aad467ee4157921acc1ae2a25cf8d367c622cc
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
@@ -19,26 +16,16 @@ import android.view.ViewGroup;
 import com.e.hospoclear.Adapters.TabAdapter;
 import com.e.hospoclear.R;
 import com.google.android.material.tabs.TabLayout;
-<<<<<<< HEAD
-
-=======
->>>>>>> a2aad467ee4157921acc1ae2a25cf8d367c622cc
 
 public class HomeFragment extends Fragment {
 
-<<<<<<< HEAD
     TabLayout tabLayout ;
     ViewPager viewPager ;
-
-=======
-    TabLayout tabLayout;
-    ViewPager viewPager;
->>>>>>> a2aad467ee4157921acc1ae2a25cf8d367c622cc
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-<<<<<<< HEAD
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
@@ -78,10 +65,7 @@ public class HomeFragment extends Fragment {
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-=======
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
->>>>>>> a2aad467ee4157921acc1ae2a25cf8d367c622cc
         return view;
     }
 }
